e4c11a0dcfc4009d34a36010304f6bbf1f7017bb
Shaunw0814 <73918962+Shaunw0814@users.noreply.github.com>
2021-08-14 22:29:09 -0400

    Updated ballBounce()

--- myScript.js
+++ myScript.js
@@ -2,40 +2,70 @@ window.onload = function ballBounce(){
     
     let id = null;
     clearInterval(id);
-    id = setInterval(frame, 5)
+    id = setInterval(frame, 8)
 
     var ball = document.getElementById("animationCircle")
     var area = document.getElementById("animationContainer")
     var maxX = area.clientWidth;
     var maxY = area.clientHeight;
-    var posX = 0;
-    var posY = 0; 
-    var randomX = Math.floor((Math.random() * 3)+1);
-    var randomY = Math.floor((Math.random() * 3)+1);
+    var posX = Math.floor(Math.random()*maxX - 2) + 2;
+    var posY = Math.floor(Math.random()*maxY - 2) + 2; 
+    ball.style.top = posY + 'px';
+    ball.style.left = posX + 'px';
 
-    let checkX = 0;
-    let checkY = 0;
+    //making colors
+    let blueColor = "#00d9ffc9";
+    let greenColor = "#00ff55c9";
+    let yellowColor = "#eeff00";
+    let orangeColor = "#ff7300c9";
+    let redColor = "#ff0000c9";
+
+    const colors = [blueColor, greenColor, yellowColor, orangeColor, redColor];
+
+    //randomizers
+    let colorPicker = 0;
+    let checkX = Math.floor((Math.random() * 2));
+    let checkY =  Math.floor((Math.random() * 2));
+    let randomX = Math.floor((Math.random() * 2)+1);
+    let randomY = Math.floor((Math.random() * 2)+1);
 
     function frame(){
+        ball.style.backgroundColor = colors[colorPicker];
         maxX = area.clientWidth;
         maxY = area.clientHeight;
         if(posX >= maxX){
             checkX = 1;
             randomX = Math.floor((Math.random() * 2)+1);
+            colorPicker++;
+            if(colorPicker == colors.length){
+                colorPicker = 0;
+            }
         }
 
         if(posY >= maxY){
             checkY = 1;
             randomY = Math.floor((Math.random() * 2)+1);
+            colorPicker++;
+            if(colorPicker == colors.length){
+                colorPicker = 0;
+            }
         }
 
         if(posX < 0){
             checkX = 0;
             randomX = Math.floor((Math.random() * 2)+1);
+            colorPicker++;
+            if(colorPicker == colors.length){
+                colorPicker = 0;
+            }
         }
         if(posY < 0){
             checkY = 0;
             randomY = Math.floor((Math.random() * 2)+1);
+            colorPicker++;
+            if(colorPicker == colors.length){
+                colorPicker = 0;
+            }
         }
 
     
